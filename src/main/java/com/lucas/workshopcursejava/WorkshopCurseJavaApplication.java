7a575ba99ef34e4eb1d7ff0e567b6f44a9e4c71c
lucassantos-dev <jl.lucassantos098@gmail.com>
2024-05-20 10:39:42 -0300

    feat:initial commit

---
+++ src/main/java/com/lucas/workshopcursejava/WorkshopCurseJavaApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.workshopcursejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopCurseJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopCurseJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucas/workshopcursejava/WokshopCurseJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.workshopcursejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WokshopCurseJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
