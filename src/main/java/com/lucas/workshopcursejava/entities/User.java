44e0fff05cae39160318a806a938650fc4bb86d6
lucassantos-dev <jl.lucassantos098@gmail.com>
2024-05-20 13:23:52 -0300

    feat:entity order

--- src/main/java/com/lucas/workshopcursejava/entities/User.java
+++ src/main/java/com/lucas/workshopcursejava/entities/User.java
@@ -3,6 +3,8 @@ package com.lucas.workshopcursejava.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 
@@ -17,6 +19,10 @@ public class User implements Serializable {
     private String phone;
     private String password;
 
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+
+
     public User() {
     }
 
@@ -26,6 +32,11 @@ public class User implements Serializable {
         this.email = email;
         this.phone = phone;
         this.password = password;
+    }
+
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     public Long getId() {
