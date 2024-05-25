ee272c402f3f6a82c453c4b560e3b24656d5d5ff
lucassantos-dev <jl.lucassantos098@gmail.com>
2024-05-25 10:37:13 -0300

    feat:Get exceptions

--- src/main/java/com/lucas/workshopcursejava/services/UserService.java
+++ src/main/java/com/lucas/workshopcursejava/services/UserService.java
@@ -2,6 +2,7 @@ package com.lucas.workshopcursejava.services;
 
 import com.lucas.workshopcursejava.entities.User;
 import com.lucas.workshopcursejava.repositories.UserRepository;
+import com.lucas.workshopcursejava.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,7 @@ public class UserService {
     }
     public  User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
     public  User insert(User obj){
         return  repository.save(obj);
