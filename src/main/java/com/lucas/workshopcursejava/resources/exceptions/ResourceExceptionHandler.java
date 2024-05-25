6662311a73d3caaf6d4790742dc4a93c13707468
lucassantos-dev <jl.lucassantos098@gmail.com>
2024-05-25 10:54:50 -0300

    feat:Delete exceptions

--- src/main/java/com/lucas/workshopcursejava/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/lucas/workshopcursejava/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.lucas.workshopcursejava.resources.exceptions;
 
+import com.lucas.workshopcursejava.services.exceptions.DatabaseException;
 import com.lucas.workshopcursejava.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -16,6 +17,13 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
         String error = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+        String error = "Data error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
--- src/main/java/com/lucas/workshopcursejava/services/UserService.java
+++ src/main/java/com/lucas/workshopcursejava/services/UserService.java
@@ -2,8 +2,11 @@ package com.lucas.workshopcursejava.services;
 
 import com.lucas.workshopcursejava.entities.User;
 import com.lucas.workshopcursejava.repositories.UserRepository;
+import com.lucas.workshopcursejava.services.exceptions.DatabaseException;
 import com.lucas.workshopcursejava.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -26,7 +29,13 @@ public class UserService {
         return  repository.save(obj);
     }
     public void  delete(Long id){
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
     public  User update(Long id, User obj){
         User entity = repository.getReferenceById(id);
