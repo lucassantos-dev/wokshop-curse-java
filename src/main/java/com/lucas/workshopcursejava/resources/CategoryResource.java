3d828671a797d660e68081022bc676a042b8db0a
lucassantos-dev <jl.lucassantos098@gmail.com>
2024-05-25 09:56:51 -0300

    feat:Create user

--- src/main/java/com/lucas/workshopcursejava/resources/CategoryResource.java
+++ src/main/java/com/lucas/workshopcursejava/resources/CategoryResource.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/categories/")
+@RequestMapping("/categories")
 public class CategoryResource {
     @Autowired
     private CategoryService service;
--- src/main/java/com/lucas/workshopcursejava/resources/ProductResource.java
+++ src/main/java/com/lucas/workshopcursejava/resources/ProductResource.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/products/")
+@RequestMapping("/products")
 public class ProductResource {
     @Autowired
     private ProductService service;
